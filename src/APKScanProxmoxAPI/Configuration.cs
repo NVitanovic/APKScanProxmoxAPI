cbaf8a6201d48cfc0a796803b7f8c2064e6b31ee
NVitanovic <x-team@live.com>
2017-01-16 18:13:15 +0100

    changed configuration file a little bit more, added config for allowed tokens and a config for the channel which will be listened to

---
+++ src/APKScanProxmoxAPI/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APKScanSharedClasses;
+namespace APKScanProxmoxAPI
+{
+    public class Configuration : SharedConfiguration
+    {
+        public List<string> authorized { get; set; }
+        public string message_channel { get; set; }
+    }
+}
--- src/APKScanProxmoxAPI/Program.cs
+++ src/APKScanProxmoxAPI/Program.cs
@@ -5,20 +5,22 @@ using System.Threading.Tasks;
 using APKScanSharedClasses;
 using System.IO;
 using Newtonsoft.Json;
+using StackExchange.Redis;
 
 namespace APKScanProxmoxAPI
 {
     public class Program
     {
         //-------------------------------------------------------------------------------------------------------------------------------
-        public static SharedConfiguration config { get; set; }
+        public static   Configuration           config  { get; set; }
+        private static  DataLayer               dl      = null;
         //-------------------------------------------------------------------------------------------------------------------------------
         private static bool LoadConfiguration()
         {
             if (!File.Exists("configuration.json"))
                 return false;
             string data = File.ReadAllText("configuration.json");
-            config = JsonConvert.DeserializeObject<SharedConfiguration>(data);
+            config = JsonConvert.DeserializeObject<Configuration>(data);
             if (config == null)
                 return false;
             return true;
@@ -26,11 +28,16 @@ namespace APKScanProxmoxAPI
         //-------------------------------------------------------------------------------------------------------------------------------
         public static void Main(string[] args)
         {
+            //Load the configuration
             if(!LoadConfiguration())
             {
                 Console.WriteLine("Error while reading the configuration file!");
                 return;
             }
+            //init DB Connection
+            dl = DataLayer.getInstance();
+            
+            //init subscriber
 
             Console.ReadLine();
         }
